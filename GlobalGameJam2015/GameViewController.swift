53471d6cff3260b3a65624396d99fb94305e0fcb
Kuba Miloszewski <kuba.miloszewski@gmail.com>
2015-01-23 22:24:19 +0100

    The app only supports the iPhone in Portrait orientation. Removed the sample code from the game view controller.

--- GlobalGameJam2015/GameViewController.swift
+++ GlobalGameJam2015/GameViewController.swift
@@ -27,43 +27,37 @@ extension SKNode {
 
 class GameViewController: UIViewController {
 
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
+            
             let skView = self.view as SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
             
-            /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
         }
     }
+    
+    // MARK: Status bar
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    // MARK: View rotation
 
     override func shouldAutorotate() -> Bool {
-        return true
+        return false
     }
 
     override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-        } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
+        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
     }
 }
