1ffac8acdc4f5ac84af1d7cf87cbfa3c0cc474b9
Kuba Miloszewski <kuba.miloszewski@gmail.com>
2015-01-24 09:46:24 +0100

    Added left & right walls to the scene (sort of).

--- GlobalGameJam2015/GameScene.swift
+++ GlobalGameJam2015/GameScene.swift
@@ -10,8 +10,34 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var leftWall: SKSpriteNode?
+    var rightWall: SKSpriteNode?
+    
     override func didMoveToView(view: SKView) {
-        
+        createSceneContents(view)
     }
     
+    func createSceneContents(view: SKView) {
+        let wallWidth = CGFloat(35)
+        let wallHeight = view.frame.height
+        
+        createLeftWall(view, wallWidth: wallWidth, wallHeight: wallHeight)
+        createRightWall(view, wallWidth: wallWidth, wallHeight: wallHeight)
+    }
+    
+    func createLeftWall(view: SKView, wallWidth: CGFloat, wallHeight: CGFloat) {
+        var wallNode = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: wallWidth, height: wallHeight))
+        wallNode.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: 0, y: 0, width: wallWidth, height: CGRectGetHeight(view.frame)))
+        wallNode.position = CGPoint(x: CGRectGetWidth(wallNode.frame)/2, y: CGRectGetHeight(view.frame)/2)
+        addChild(wallNode)
+        leftWall = wallNode
+    }
+    
+    func createRightWall(view: SKView, wallWidth: CGFloat, wallHeight: CGFloat) {
+        var wallNode = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: wallWidth, height: wallHeight))
+        wallNode.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: CGRectGetWidth(view.frame) - CGRectGetWidth(view.frame), y: 0, width: wallWidth, height: CGRectGetHeight(view.frame)))
+        wallNode.position = CGPoint(x: CGRectGetWidth(view.frame) - CGRectGetWidth(wallNode.frame)/2, y: CGRectGetHeight(view.frame)/2)
+        addChild(wallNode)
+        rightWall = wallNode
+    }
 }
