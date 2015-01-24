1f0206eb1b2079ff9908a03a6c655fd26128e276
Tomasz Bąk <tomasz.bak2@gmail.com>
2015-01-24 14:28:44 +0100

    Body moving up.

--- GlobalGameJam2015/GameScene.swift
+++ GlobalGameJam2015/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
     var selected: Leg?
     var body: SKSpriteNode?
     var recon: Bool?
+    var leftLegs: [Leg]?
+    var rightLegs: [Leg]?
     
     var leftWall: SKSpriteNode?
     var rightWall: SKSpriteNode?
@@ -28,6 +30,8 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
     
     override func didMoveToView(view: SKView) {
         nodes = []
+        leftLegs = []
+        rightLegs = []
         createSceneContents(view)
         createSpiderLegs()
         createBody()
@@ -44,8 +48,10 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
     func createSpiderLegs() {
         let div = [1.0/3.0, 1.0/2.0, 2.0/3.0]
         for val in div {
-            createLegs(CGPointMake(30, self.size.height * CGFloat(val)))
-            createLegs(CGPointMake(self.size.width-30, self.size.height * CGFloat(val)))
+            let left = createLegs(CGPointMake(30, self.size.height * CGFloat(val)))
+            let right = createLegs(CGPointMake(self.size.width-30, self.size.height * CGFloat(val)))
+            leftLegs?.append(left)
+            rightLegs?.append(right)
         }
     }
     
@@ -60,13 +66,14 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
         letThereBeLight(view)
     }
     
-    func createLegs(start: CGPoint) {
+    func createLegs(start: CGPoint) -> Leg {
         let end = CGPointMake(self.size.width / 2, self.size.height / 2)
         let node = createLegAtPoints(start, end:end)
         self.addChild(node)
+        return node
     }
 
-    func createLegAtPoints(start: CGPoint, end: CGPoint) -> SKSpriteNode {
+    func createLegAtPoints(start: CGPoint, end: CGPoint) -> Leg {
         let leg = Leg(texture: SKTexture(imageNamed: "noga"))
         leg.setupHandle()
         leg.moveEnd(end)
@@ -122,18 +129,7 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
     func hadle(sender: UIPanGestureRecognizer) {
         let location = sender.locationInView(sender.view)
         let location2 = self.convertPointToView(location)
-        
-        switch sender.state {
-            case .Changed:
-                selected?.moveToPoint(location2)
-            case .Ended:
-                selected?.handle?.color = UIColor.blueColor()
-                selected = nil
-                recon = true
-            default:
-                break
-        }
-
+        var leave = false
         
         for test in nodesAtPoint(location2) {
             let handle = test as SKSpriteNode
@@ -148,10 +144,54 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
                         default:
                         break
                     }
+                    leave = true
                     break
                 }
             }
+            if leave {
+                break
+            }
         }
+        
+        if selected == nil {
+            return
+        }
+        
+        switch sender.state {
+        case .Changed:
+            selected?.moveToPoint(location2)
+        case .Ended:
+            if let sel = selected {
+                if contains(leftLegs!, sel) {
+                    sel.moveToPoint(CGPointMake(30, selected!.point!.y))
+                } else {
+                    sel.moveToPoint(CGPointMake(self.size.width - 30, selected!.point!.y))
+                }
+            }
+            
+            selected?.handle?.color = UIColor.blueColor()
+            selected = nil
+            recon = true
+            body?.removeActionForKey("bounce")
+            body?.position = correctBodyPosition()
+        default:
+            break
+        }
+    }
+    
+    func correctBodyPosition() -> CGPoint {
+        var avr1 = CGFloat(0);
+        var avr2 = CGFloat(0);
+        for leg in leftLegs! {
+            avr1 += leg.point!.y
+        }
+        for leg in rightLegs! {
+            avr2 += leg.point!.y
+        }
+        avr1 /= CGFloat(leftLegs!.count)
+        avr2 /= CGFloat(rightLegs!.count)
+        
+        return CGPointMake(self.size.width / 2, (avr1 + avr2) / 2.0)
     }
     
     func hadle2(sender: UIPanGestureRecognizer) {
@@ -164,15 +204,13 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
         for test in nodesAtPoint(location2) {
             if body! === test {
                 switch sender.state {
+                    case .Began:
+                        body?.removeActionForKey("bounce")
                     case .Changed:
                         body?.position = location2
-                        for leg in nodes! {
-                            leg.moveEnd(location2)
-                        }
                     case .Ended:
-                        let move = SKAction.moveTo(CGPointMake(self.size.width / 2, self.size.height / 2), duration: 0.5)
-                        let move2 = SKAction.moveTo(CGPointMake(self.size.width / 2, self.size.height / 2), duration: 1, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0)
-                        body?.runAction(move2)
+                        let move = SKAction.moveTo(correctBodyPosition(), duration: 1, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0)
+                        body?.runAction(move, withKey: "bouce")
                     
                 default:
                     break
